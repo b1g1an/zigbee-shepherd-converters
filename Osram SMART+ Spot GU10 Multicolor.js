8f22437da5116054cad13b43ca6942092732eb3a
b1g1an <ian@fizzyworms.co.uk>
2018-09-27 23:35:57 +0100

    Add files via upload

---
+++ Osram SMART+ Spot GU10 Multicolor.js
@@ -0,0 +1,9 @@
+    {
+        zigbeeModel: ['PAR 16 50 RGBW - LIGHTIFY'],
+        model: 'AB35996',
+        vendor: 'OSRAM',
+        description: 'SMART+ Spot GU10 Multicolor',
+        supports: generic.light_onoff_brightness_colortemp_colorxy().supports,
+        fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
+    },
